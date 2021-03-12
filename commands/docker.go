6eb257c096069665535f8a4b2ff838c83880b995
Simone Margaritelli <evilsocket@gmail.com>
2021-03-12 17:56:16 +0100

    new: docker:push

--- commands/docker.go
+++ commands/docker.go
@@ -18,6 +18,12 @@ func init() {
 		Logic:      dockerBuild,
 	}
 
+	Available["docker:push"] = &Command{
+		Identifier: "docker:push",
+		Argc:       1,
+		Logic:      dockerPush,
+	}
+
 	Available["docker:create_tag"] = &Command{
 		Identifier: "docker:create_tag",
 		Argc:       3,
@@ -44,6 +50,25 @@ func dockerBuild(env *Environment, args ...string) error {
 	msg("docker", "building %s ...\n", tagName)
 
 	if err = do(env.Dry, docker, "build", "-t", tagName, dockerfile); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func dockerPush(env *Environment, args ...string) error {
+	docker, err := exec.LookPath("docker")
+	if err != nil {
+		return err
+	}
+
+	targetName := args[0]
+	if repo := env.Vars["DOCKER_REPOSITORY"]; repo != "" {
+		targetName = fmt.Sprintf("%s/%s", repo, targetName)
+	}
+
+	msg("docker", "pushing %s ...\n", targetName)
+	if err = do(env.Dry, docker, "push", targetName); err != nil {
 		return err
 	}
 
